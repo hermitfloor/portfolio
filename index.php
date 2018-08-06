f8de3825e6a5d32aba6c9114600213e352259cd5
Jen Law <41809098+princeyiu@users.noreply.github.com>
2018-08-06 15:45:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,85 +5,81 @@
 <div id="info"> 
  <div id="about">
 <div id="aboutText"> 
-<p>Hello&#33; I'm Jen and I've lived on the Web since my father got our very first computer with a giant CRT monitor that I could never lift.</p>
+<p>Hello! I'm Jen and I've lived on the Web since my father got our very first computer with a giant CRT monitor that I could never lift.</p>
 <p>I've been designing websites as a hobby since 2001, making clunky and garish layouts that were popular at the time using free hosting sites such as Geocities and Freewebs.<br /> Years later, 
 I have continued to learn and develop personal sites and editing themes on blog sites.</p>
 <p>I originally came from an art background so I have an extensive interest in many branches such as Illustration, Graphic Design, Animation, Tattoo Work, Sculpture, Toy Customization and Photography. </p>
 <p>Main hobbies I dabble with are customizing and sculpting art dolls and collecting tattoos and toys! </p>
-<p>I also have <a href="img/boys.JPG" id="myImg">two cats</a> called Geoff and Artemis and a duprasi named Neil Duprasi-Tyson. I love them very much!</p>
+<p>I also have <a href="http://instagram.com/artemis.law" target="_blank">two cats</a> called Geoff and Artemis and a duprasi named Neil Duprasi-Tyson. I love them very much!</p>
 
   <div id="iconbar">
-  <a href="https://www.linkedin.com/in/jennifer-law-46b595169/" target="_blank" title="LinkedIn"><span id="icons" class="fab fa-linkedin-in fa-3x" alt="LinkedIn"></span></a>
+  <a href="https://www.linkedin.com/in/jennifer-law-46b595169/" target="_blank" title="LinkedIn"><span id="icons" class="fab fa-linkedin-in fa-3x"></span></a>
 <a href="https://twitter.com/yiu" target="_blank" title="Twitter"><span id="icons" class="fab fa-twitter fa-3x"></span></a> 
   <a href="https://instagram.com/hoboprince" target="_blank" title="Instagram"><span id="icons" class="fab fa-instagram fa-3x"></span></a> 
   <a href="https://github.com/hoboprince" target="_blank" title="GitHub"><span id="icons" class="fab fa-github-alt fa-3x"></span></a>
   <a href="https://facebook.com/lawsyj" target="_blank" title="Facebook"><span id="icons" class="fab fa-facebook-f fa-3x"></span></a> 
     <a href="https://flickr.com/photos/yiuu" target="_blank" title="Flickr"><span id="icons" class="fab fa-flickr fa-3x"></span></a>
   </div> 
-  <p><a class="button" href="mailto:hi@lawsy.uk" target="_top" title="hi@lawsy.uk" alt="hi@lawsy.uk">SEND ME AN EMAIL</a> </p> </div></div> </div> 
+  <p><a class="button" href="mailto:hi@lawsy.uk" target="_top" title="hi@lawsy.uk">SEND ME AN EMAIL</a> </p> </div></div> </div> 
   
   <section id="portfolio"> 
   <h1>Projects</h1>
 <div class="row">
   <div class="column">
-    <div class="content">
-      <img src="/img/stillchrome.png" alt="Personal Website Mockup" style="width:100%">
-      <h3>Personal Site Mockup</h3>
-      <p>A Photoshop mockup of my previous personal site which I updated to become this portfolio site.</p>  
-      <p><a class="portfolio" href="" target="_blank">live view</a> // <a href="" target="_blank">code</a></p>
-        </div>
+    <div class="thumbnail"><img id="image" src="/img/stillchrome.png" alt=""></div>
+    <h2>Personal Site Mockup</h2>
+      <p>A mockup of my previous personal site which I updated to become this portfolio site. Created in Photoshop.</p>  
+       <a href="/img/stillchrome.png" target="_blank">view</a> </div>
+  <div class="column">
+    <div class="thumbnail"><img src="/img/hello.png" alt=""></div>
+      <h2>Portfolio Site</h2>
+      <p>This portfolio site you see now was created in a week. Created with HTML5 and CSS3.</p>
+  <a href="https://github.com/princeyiu/portfolio" target="_blank">view code</a>
   </div>
   <div class="column">
-    <div class="content">
-      <img src="/img/portfolio.jpg" alt="Portfolio Site" style="width:100%">
-      <h3>Portfolio Site</h3>
-      <p>This portfolio site you see now was created in a week.</p>
-      <p><a href="" target="_blank">live view</a> // <a href="" target="_blank">code</a></p>
-    </div>
-  </div>
-  <div class="column">
-    <div class="content">
-      <img src="/img/pokemon.png" alt="Pokemon in Python" style="width:100%">
-      <h3>Pokemon Battle Simulator</h3>
+    <div class="thumbnail"><img src="/img/pokemon.png" alt=""></div>
+      <h2>Pokemon Battle Simulator</h2>
       <p>Coding a simulated Pokemon battle in Python. A learning project between my partner and I.</p> 
-      <p><a href="" target="_blank">live view</a> // <a href="" target="_blank">code</a></p>
+    <a href="https://github.com/princeyiu/pokemon-python" target="_blank">view code</a>
     </div>
-  </div>
-  <div class="column">
-    <div class="content">
-      <img src="/img/dollcomm.png" alt="Doll Commissions Site" style="width:100%">
-      <h3>Doll Commissions Page</h3>
+     <div class="column">
+    <div class="thumbnail"><img src="/img/dollcomm.png" alt=""></div>
+      <h2>Commissions Page</h2>
       <p>A static webpage for my doll commission services.</p> 
-      <p><a href="" target="_blank">live view</a> // <a href="" target="_blank">code</a></p>
+  <a href="/dollcomm.html" target="_blank">live view</a> // <a href="https://github.com/princeyiu/portfolio/blob/master/dollc" target="_blank">code</a>
     </div>
-  </div>
   <div class="column">
-<div class="content">
-  <img src="/img/dozing.png" alt="Bear" style="width:100%">
-  <h3>Some Other Work</h3>
+    <div class="thumbnail"><img src="/img/tumblr.png" alt=""></div>
+  <h2>Tumblr Theme</h2>
+  <p>A Tumblr blog theme edited from a free default theme.</p>
+ <a href="" target="_blank">live view</a> // <a href="" target="_blank">code</a>
+    </div>  
+  <div class="column">
+    <div class="thumbnail"><img src="/img/dozing.png" alt=""></div>
+  <h2>Some Other Work</h2>
   <p>Lorem ipsum ormeomflasmkdasndknasnakla..</p>
-  <p><a href="" target="_blank">live view</a> // <a href="" target="_blank">code</a></p>
-    </div></div>
-  </div>
+ <a href="" target="_blank">live view</a> // <a href="" target="_blank">code</a>
+    </div>
+    </div>
 </section>
 
-<section id="gallery"> <h1>Gallery</h1>
+<section id="gallery"> <h1>Hobbies</h1>
 <div class="row"> 
   <div class="column">
-    <img src="/img/bomnal.jpg" style="width:100%">
-       <img src="/img/heads.jpg" style="width:100%">
-    <img src="/img/gumi.jpg" style="width:100%">
+    <img src="/img/bomnal.jpg" alt="Bomnal" style="width:100%">
+       <img src="/img/heads.jpg" alt="BJD heads" style="width:100%">
+    <img src="/img/gumi.jpg" alt="Gumi" style="width:100%">
   </div>
   <div class="column">
-    <img src="/img/avory.jpg" style="width:100%">
-    <img src="/img/sunrifin.jpg" style="width:100%">
-    <img src="/img/hands.jpg" style="width:100%">
+    <img src="/img/avory.jpg" alt="Commission-Avory" style="width:100%">
+    <img src="/img/sunrifin.jpg" alt="Commission-Sunri" style="width:100%">
+    <img src="/img/hands.jpg" alt="Hands" style="width:100%">
   
   </div>
    <div class="column">
-       <img src="/img/barley.jpg" style="width:100%">
-    <img src="/img/birb.jpg" style="width:100%">       
-    <img src="/img/sol.png" style="width:100%">
+       <img src="/img/barley.jpg" alt="Barley" style="width:100%">
+    <img src="/img/birb.jpg" alt="Red whiskered Bulbul" style="width:100%">       
+    <img src="/img/sol.png" alt="Sol" style="width:100%">
   </div>   
   </div>
  </section>
